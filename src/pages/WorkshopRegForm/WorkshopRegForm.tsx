a190635e417016e0f69e395fcdae560df3ca0c76
kaushik-codemonkey <kaushiktechie03@gmail.com>
2024-03-29 15:26:13 +0530

    Fix styling

--- src/pages/WorkshopRegForm/WorkshopRegForm.tsx
+++ src/pages/WorkshopRegForm/WorkshopRegForm.tsx
@@ -1,4 +1,4 @@
-import React, { EventHandler, FormEvent, FormEventHandler, useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import styles from "./workshop-reg-form.module.scss"
 import { fetchExpertises } from '../../service/vcs-service/vcs-service';
 import { useParams } from 'react-router-dom';
@@ -10,10 +10,24 @@ const WorkshopRegForm = () => {
         e.preventDefault();
         console.log({ formData });
     }
-    const fields = [{ key: "name", label: "Name" },
-    { key: "email", label: "Email" },
+    const genderOptions = [{
+        key: 'male',
+        label: "Male"
+    },
+    {
+        key: 'female',
+        label: "Female"
+    }]
+    const fields = [{ key: "name", label: "Full Name" },
+    { key: 'dob', label: "Date of birth", type: 'date' },
+    { key: 'gender', label: 'Gender', type: 'dropdown', options: genderOptions },
+    { key: "address", label: "Address" },
+    { key: "city", label: "City" },
+    { key: "state", label: "State/Province" },
     { key: "country", label: "Country" },
-    { key: "phone", label: "Phone" },
+    { key: "phone", label: "Mobile number" },
+
+    { key: "email", label: "Student E-Mail address" },
     { key: "course", label: "Course" },
     { key: "profession", label: "Profession" },
     { key: "designation", label: "Designation" },
@@ -30,22 +44,43 @@ const WorkshopRegForm = () => {
     }, [])
 
     return (
-        <form onSubmit={submitHandler} onChange={(e: React.FormEvent<HTMLFormElement>) => {
-            const val = (e.target as any).value
-            const name = (e.target as any).name
-            setFormData(formDat => ({ ...formDat, [name]: val }))
-        }}
-            className={styles.workshopFormContainer}>
-            {fields.map((fieldVal) => {
-                return (
-                    <label className={styles.workshopFormFields} key={fieldVal.key}>
-                        {fieldVal.label}
-                        <input type="text" name={fieldVal.key} />
-                    </label>
-                )
-            })}
-            <button>Submit</button>
-        </form>
+        <div className={styles.workshopPageContainer}>
+            <div className={styles.workshopBannerImage}>
+                <img src="https://picsum.photos/seed/picsum/200/300" />
+            </div>
+            <div className={styles.workshopSection2Container}>
+                <div className={styles.workshopFormTitleContainer}>
+                    <label>Excel workshop</label>
+                </div>
+                <form
+                    onSubmit={submitHandler}
+                    className={styles.workshopFormContainer}
+                    onChange={(e: React.FormEvent<HTMLFormElement>) => {
+                        const val = (e.target as any).value
+                        const name = (e.target as any).name
+                        setFormData(formDat => ({ ...formDat, [name]: val }))
+                    }}
+                >
+                    {fields.map((fieldVal) => {
+                        if (fieldVal.type === "dropdown") {
+                            return <label className={styles.workshopFormFields} key={fieldVal.key}>
+                                {fieldVal.label}
+                                <select name={fieldVal.key}>
+                                    {fieldVal.options?.map(option => <option value={option.key}>{option.label}</option>)}
+                                </select>
+                            </label>
+                        }
+                        return (
+                            <label className={styles.workshopFormFields} key={fieldVal.key}>
+                                {fieldVal.label}
+                                <input type={fieldVal.type || "text"} name={fieldVal.key} />
+                            </label>
+                        )
+                    })}
+                    <button>Submit</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
